997fb7a146b8e0b3796dd74bb79786d8a32c4807
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-06-13 03:30:07 +0800

    Oopsie

--- dapp/app/page.tsx
+++ dapp/app/page.tsx
@@ -4,14 +4,25 @@ import NavBar from "@/components/NavBar";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Footer from "@/components/Footer";
+import { useEffect, useState, useMemo } from "react";
 
 export default function Home() {
+  const [rendered, setrendered] = useState(false);
+
+  useEffect(() => {
+    setrendered(true);
+  }, [setrendered]);
+
   return (
     <main>
-      <NavBar />
-      <Hero />
-      <About />
-      <Footer />
+      {rendered && (
+        <div>
+          <NavBar />
+          <Hero />
+          <About />
+          <Footer />
+        </div>
+      )}
     </main>
   );
 }
