626337c0e152aa54dfe1a9ba3fa9a8263325b015
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-06-13 03:20:35 +0800

    Added a few sections

--- dapp/app/page.tsx
+++ dapp/app/page.tsx
@@ -2,12 +2,16 @@
 import Image from "next/image";
 import NavBar from "@/components/NavBar";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main>
       <NavBar />
       <Hero />
+      <About />
+      <Footer />
     </main>
   );
 }
