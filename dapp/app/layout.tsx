de62689afb87964a1266f8752b6f0ac17bba407d
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-06-14 19:56:44 +0800

    Analytics

--- dapp/app/layout.tsx
+++ dapp/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${space_init.variable}`}>{children}</body>
+      <body className={`${space_init.variable}`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
